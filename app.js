9d3c9129a5f1ff6c896b2af8346b6fed6bbad4cf
Peter Christensen <pchristensen@box.com>
2021-05-13 16:31:28 +0100

    peter

--- app.js
+++ app.js
@@ -1,51 +1,35 @@
-// index.js
 
-/**
- * Required External Modules
- */
 
 require('dotenv').config()
 
 const jwtDecode = require('jwt-decode');
 
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
-var BoxSDK = require('box-node-sdk');
+const BoxSDK = require('box-node-sdk');
 
 const express = require('express');
 
-var fs = require('fs');
+const fs = require('fs');
 const path = require('path');
 
-var http = require("https");
+const http = require("https");
 
-var util = require('util');
+const util = require('util');
 
-var request = require('request');
+const request = require('request');
 const querystring = require("querystring");
 
 const expressSession = require("express-session");
 const passport = require("passport");
 const Auth0Strategy = require("passport-auth0");
 
-const boxAppSettings ={
-  "boxAppSettings": {
-    "clientID": "16y3foluw9j45cejdny8dcn99tdigw5r",
-    "clientSecret": "ca8dqxTec7GzwcCNGFV0JMIYkATaqYy3",
-    "appAuth": {
-      "publicKeyID": "b71o3jh9",
-      "privateKey": "-----BEGIN ENCRYPTED PRIVATE KEY-----\nMIIFDjBABgkqhkiG9w0BBQ0wMzAbBgkqhkiG9w0BBQwwDgQII7MT7ZaOi6gCAggA\nMBQGCCqGSIb3DQMHBAhQmOn6CpZ9gQSCBMhBKQiqPdFzLdy9Ii2j4VuVb/PoLmGq\n3Vmg4X0WWdiRDq8TEgU+Kgtm2Lhk8PcD4X3o6JOsLmPu8pZryVpCbnrT6Qc9yB4y\nBejQ3aedNKUEJ9p7IfC5j7OmRDIc55WLJYElmqJeHJjGutQf7ynNQ5xvnoZG8WpW\nyfZPw+Pijhjd247kqBYTFqhOkyl4Eo5QVuBVLTmYOO98cQGY+Mb1Bc1O9EhGfXiQ\nO/szxuofInCtjzqadLOyBma34Se+Hj18qOfweLGFjqVWPDis5YeTP6n+qb0TEdyr\nQQ+wBlLlVmnv+KAzwh+GJ08Y/FFomxWKGkuz7e6vlPnjAOEtmFisdiYBbUXDitQy\nJig0oyAXyhMG74pdCs5cWF8Fc38BfXtBZ9By812GpZl4SAw66Q1LSvClnC8FFbZp\ny1TJkjC7eJkcGo8oXXBqskGiIKjQGkbBQW1BR9FfwFUJatYkXpBfnmTSbA/0/aam\nTMTXQaTNvVRjgAg18xAiucUdIjSoIxA8H1dXXQGE/nJ1sb+3sywunGw5Bd9y8aNF\nE2F24JugAUKUDEj3lRZOSifIn3vD+wJOt1FIZZ1XSnSBvF5o5yPYrezTomMYfAa9\ntZaRze10R+65yAJv8z7hxeEfTXLK4rsyBe7J666sWgKW/VEiaiVzVkXJTIBUwwhg\nVSQlQxQ7A9VtHnJKcR0mBDi/OVeGGUwqmcS/crUDdlhq1T8GzU9m4kUun7RxrXt2\nwHCvV/3CX7fCpkgxal51TJ0+frsoyb1y8nN9kPCq3n8MHlfGTnfXYy4/oobVR4sZ\nve4OK72C11F3JVm/QcSHXhgPoWeDIrW6PGr5Wm7bWLXTELr+J+zhXPtslbKWO1J2\nbhJQe/viKRNt0QXEY6r+Ne7Cuh447ZScQtWu6i4kTXndhYfrq2uRaf9I6n1YD3Wq\nA6edqVZoL8fjraHkGNuoUZ0dSGLgzmlSDB0kKELdWuQeLUeESuWqg2J8rN8jNCPu\ne6SLx2umWPuqwHutB1404/IiC3LJDtXg/WXE8ydExb2ACS0p5J+HDTkyPHb/Gs4n\nDy6PEMnwkq+Mq2NOOGGGZOU4heVBDhEGpEG4Z1qOoWvYYkzRV7d3MM3CFZ/ElbB7\nGNG4ed14HDLLsBcsWg5LnSdTr6fjOyVM29ev10P8MqaJawwBB6sLeUyGUEsSPpwu\n1bjuuLioUITo51Bn6X5LTVkhHrrp9OTg03Klh1cPWVVOtSjXQFt8aXEIXbmPq8O+\n8FJJO5hXX/CSnC6NLYoI3rcnAhY0RFVtBZkYtYrav1N9U35dkbInaYJ4lkEgn7Hs\ngg0fG/TbPGyO0Jmj8EpwrRcyBxcpfpsymszb4zDX0nOpXSLRqslhqNrXjcdlgT7v\nr03QdSUTbaKrM97AM76ao05uN3+MGOlYsP0SMBeqjoWWb0kKNizsF8LWw0EXzA3M\nKI0zv6u+JzrRMQSbD5QMoIZRoZJ2eTcO86DYGBDDpFfQbAuX9VMJw31PIKmnb+3M\n43VNHkFC/LJl5PSU4+6GEfPGb6fb9/Kx1niXtLf6Qt//offwBZOjoGxKzGyYcKtd\naZXk0slwjDianjaFg7Hs1ONDfYvOrsF/loFTHqEQbdzEbJ6txfB49+bJB62B/u3q\nIVk=\n-----END ENCRYPTED PRIVATE KEY-----\n",
-      "passphrase": "ee7829475047aa584715b5a4b26db64e"
-    }
-  },
-  "enterpriseID": "47757585"
-}
+const boxAppSettings =process.env.BOX_JWT;
 
-let serviceAccountClient;
 
 console.log("Using config file...");
-let boxSession = BoxSDK.getPreconfiguredInstance(boxAppSettings);
-serviceAccountClient = boxSession.getAppAuthClient('enterprise');
+let boxSession = BoxSDK.getPreconfiguredInstance(JSON.parse(boxAppSettings));
+let serviceAccountClient = boxSession.getAppAuthClient('enterprise');
 
 
 /**
@@ -69,14 +53,7 @@ const strategy = new Auth0Strategy(
 		callbackURL: process.env.AUTH0_CALLBACK_URL
 	},
 	function (accessToken, refreshToken, extraParams, profile, done) {
-	  /**
-	   * Access tokens are used to authorize users to an API
-	   * (resource server)
-	   * accessToken is the token to call the Auth0 API
-	   * or a secured third-party API
-	   * extraParams.id_token has the JSON Web Token
-	   * profile has all the information from the user
-	   */
+	 
 		return done(null, profile);
 	}
 );
@@ -98,7 +75,7 @@ app.listen(port, function () {
 	console.log('App listening on port ' + port + '...');
 })
 app.use(expressSession({
-	secret: 'cat',
+	secret: 'box',
 	cookie: {},
 	resave: false,
 	saveUninitialized: false
@@ -120,9 +97,9 @@ app.post('/boxUI', function (req, res) {
   let auth0Id= req.body.userId;
 	getAppUserID(auth0Id)
 		.then((appUserID) => {
-			//If the box user is not present in AZURE, throw error
+			//If the box user is not present in auth0 at this point, throw error - user should have been created
 			if (!appUserID || appUserID=='NOTFOUND') {
-				res.json({error: "some error"});
+				res.json({error: "some error involving app user not found"});
 			}
 			else {
 				console.log(`App User ID is: ${appUserID.id}`);
@@ -153,9 +130,6 @@ app.get("/login", passport.authenticate("auth0", {
 	scope: "openid email profile"
 }),
 	(req, res) => {
-		
-    console.log("REQ:" + req);
-    console.log('Login was called in the Sample');
     res.redirect('/');
 	}
 );
@@ -173,7 +147,7 @@ app.get("/callback", (req, res, next) => {
 			}
 			const returnTo = req.session.returnTo;
 			delete req.session.returnTo;
-			console.log('We received a return from Auth0. - create here!' + JSON.stringify(user));
+			console.log('We received a return from Auth0. - create here if not found -> lazy instantiation' + JSON.stringify(user));
 			getAppUserID(user.user_id).then((appuserId) => {
 				if(appuserId=='NOTFOUND') {
 					createAppUser(user.user_id,user.user_name).then((appUserID) => {
@@ -218,20 +192,7 @@ app.get("/logout", (req, res) => {
 var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-
-
-/**
- *  App Configuration
- */
-
-/**
- * Routes Definitions
- */
-
-/**
- * Server Activation
- */
-
+//Creates the app user
 const createAppUser = (auth0Id,name) =>   {
   console.log(auth0Id + ":" + name);
   return serviceAccountClient.enterprise.addAppUser(name, { "is_platform_access_only": true,"external_app_user_id":auth0Id }).then((result) => {
@@ -241,6 +202,7 @@ const createAppUser = (auth0Id,name) =>   {
       }
     );
 }
+//Finds  the app user - although it can only return a single record it still returns an array
 const getAppUserID = (auth0Id) => {
   console.log("Finding extID:" + auth0Id);
   return serviceAccountClient.enterprise.getUsers({ "external_app_user_id": auth0Id,"fields":"id,name,login,external_app_user_id" })
