e50c2f29d2d1dc48cb5dc671afb8b3ff26379c4d
pchristensenB <38443139+pchristensenB@users.noreply.github.com>
2021-05-28 09:40:46 +0100

    peter

--- app.js
+++ app.js
@@ -66,8 +66,8 @@ app.set('view engine', 'ejs');
 var port = process.env.PORT || 3000;
 
 app.use(express.static('public'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.set('views', path.join(__dirname, 'views'));
 app.listen(port, function () {
 	console.log('App listening on port ' + port + '...');
